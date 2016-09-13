a8c41ca6e58ed52fa210fb3979e21a2ae0100197
thePLO ENTI <pdemiguel@enti.cat>
2016-09-13 16:39:53 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*function goTo(cl){
+	//style
+	document.querySelector("body").classList=cl;
+	//active class
+	while(document.querySelector(".active")) document.querySelector(".active").classList.remove("active");
+	document.querySelector(".ico."+cl).classList.add("active");
+	document.querySelector("li."+cl).classList.add("active");
+	//header
+
+	//content
+	//--
+}*/
